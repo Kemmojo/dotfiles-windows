5b9a8df4412a541a884531b88b2e16d7773fdbd9
Kemmojo <kemnadej81@yahoo.de>
2019-10-13 20:43:59 +0200

    bootstrap win script -made vscode extension installation test

--- bootstrap-windows.ps1
+++ bootstrap-windows.ps1
@@ -28,92 +28,92 @@ choco.exe install firacode -y
 choco.exe install sourcecodepro -y
 
 # Developent
-choco.exe install git -y
-choco.exe install git-credential-manager-for-windows -y
-choco.exe install javaruntime -y
-choco.exe install java.jdk -y
-choco.exe install androidstudio -y
-choco.exe install postman -y
+#choco.exe install git -y
+#choco.exe install git-credential-manager-for-windows -y
+#choco.exe install javaruntime -y
+#choco.exe install java.jdk -y
+#choco.exe install androidstudio -y
+#choco.exe install postman -y
 
 # WSL ( Windows Subsystem for Linux )
-choco.exe install wsl-ubuntu-1804 -y
+#choco.exe install wsl-ubuntu-1804 -y
 
 # Browser
-choco.exe install firefox -y
-choco.exe install googlechrome -y
-choco.exe install opera -y
+#choco.exe install firefox -y
+#choco.exe install googlechrome -y
+#choco.exe install opera -y
 
 # Essentials
-choco.exe install alacritty -y
-choco.exe install keepass -y
-choco.exe install 7zip -y
-choco.exe install 7zip.commandline -y
-choco.exe install winrar -y
-choco.exe install totalcommander -y
-choco.exe install filezilla -y
-choco.exe install rufus -y
+#choco.exe install alacritty -y
+#choco.exe install keepass -y
+#choco.exe install 7zip -y
+#choco.exe install 7zip.commandline -y
+#choco.exe install winrar -y
+#choco.exe install totalcommander -y
+#choco.exe install filezilla -y
+#choco.exe install rufus -y
 
 # Sysadmin
-choco.exe install teamviewer -y
-choco.exe install nmap -y
-choco.exe install wireshark -y
+#choco.exe install teamviewer -y
+#choco.exe install nmap -y
+#choco.exe install wireshark -y
 
 # Editor & IDEs
-choco.exe install neovim -y
+#choco.exe install neovim -y
 choco.exe install vscode -y
-choco.exe install visualstudiocode-insiders -y
-choco.exe install sublimetext3
+#choco.exe install visualstudiocode-insiders -y
+#choco.exe install sublimetext3
 
 # Multimedia
-choco.exe install youtube-dl -y
-choco.exe install vlc -y
-choco.exe install spotify -y
+#choco.exe install youtube-dl -y
+#choco.exe install vlc -y
+#choco.exe install spotify -y
 
 ## Security
-choco.exe install malwarebytes -y
+#choco.exe install malwarebytes -y
 
 # Gaming & Communication
-choco.exe install battle.net -y
-choco.exe install steam -y
-choco.exe install obs-studio -y
-choco.exe install teamspeak -y
-choco.exe install twitch -y
-choco.exe install discord -y
-choco.exe install whatsapp -y
-choco.exe install epicgameslauncher -y
+#choco.exe install battle.net -y
+#choco.exe install steam -y
+#choco.exe install obs-studio -y
+#choco.exe install teamspeak -y
+#choco.exe install twitch -y
+#choco.exe install discord -y
+#choco.exe install whatsapp -y
+#choco.exe install epicgameslauncher -y
 
 # Note taking
-choco.exe install joplin -y
+#choco.exe install joplin -y
 
 # Gamedevelopment
-choco.exe install unity -y
-choco.exe install blender -y
+#choco.exe install unity -y
+#choco.exe install blender -y
 
 # Mail-Client
-choco.exe install thunderbird -y
+#choco.exe install thunderbird -y
 
 # Clouds
-choco.exe install dropbox -y
-choco.exe install google-backup-and-sync -y
+#choco.exe install dropbox -y
+#choco.exe install google-backup-and-sync -y
 
 # Office
-choco.exe install libreoffice-fresh -y
+#choco.exe install libreoffice-fresh -y
 
 # Torrenting
-choco.exe install utorrent -y
-choco.exe install jdownloader -y
+#choco.exe install utorrent -y
+#choco.exe install jdownloader -y
 
 # Music development
-choco.exe install audacity -y
+#choco.exe install audacity -y
 
 # PDF Reader
-choco.exe install adobereader -y
+#choco.exe install adobereader -y
 
 # Virtual Machines
-choco.exe install virtualbox -y
+#choco.exe install virtualbox -y
 
 # Academic Paper creation
-choco.exe install texmaker -y
+#choco.exe install texmaker -y
 
 
 ################################
@@ -164,6 +164,14 @@ choco.exe install texmaker -y
 #        code --install-extension $extensions[$i]
 #    }
 #}
+$installation_block = {
+    $extensions =
+        'ms-vscode.powershell'
+
+    for ($i = 0; $i -lt $extensions.Count; $i++) {
+        code --install-extension $extensions[$i]
+    }
+}
 
 # Invoke new poweshell instance so code is in path
 Start-Process powershell -ArgumentList "-command $installation_block"
