bb0e2c24379e7fd6dfc23d62ffe7c2633c9706e7
Janik Kemnade <kemnadej81@yahoo.de>
2019-10-17 21:40:57 +0200

    Update bootstrap-wsl.sh

--- bootstrap-wsl.sh
+++ bootstrap-wsl.sh
@@ -67,6 +67,8 @@ cd $HOME
 echo "\n-----${RED} Install NodeJS / NPM / Basic Web development Packages ${NC}-----"
 sudo apt install nodejs
 sudo apt install npm
+sudo mkdir -p /usr/local/lib/node_modules
+sudo chown -R $USER /usr/local/lib/node_modules
 npm install -g sass
 npm install -g typescript
 npm install -g webpack
