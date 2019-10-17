aec593968b015914c21cd5cd3f9c1354c7182af5
Janik Kemnade <kemnadej81@yahoo.de>
2019-10-17 21:04:13 +0200

    Conditioning zsh plugins

--- bootstrap-wsl.sh
+++ bootstrap-wsl.sh
@@ -14,42 +14,57 @@ if [ ! -d "$HOME/.oh-my-zsh" ]; then
 
   # Git
   sudo apt-get install git
-
-  ## ZSH Setup
+  
   # Oh-My_Zsh Installation
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 fi
 
-echo "\n-----${RED} Install zsh-completions ${NC}-----"
-git clone https://github.com/zsh-users/zsh-completions ~/.oh-my-zsh/custom/plugins/zsh-completions
-echo "\n-----${RED} Install zsh-syntax-highlighting Plugin ${NC}-----"
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-echo "\n-----${RED} Install zsh dir-colours plugin ${NC}-----"
-git clone --recursive -j8 git://github.com/joel-porquet/zsh-dircolors-solarized ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-dircolors-solarized
-echo "\n-----${RED} Install 256color ZSH Plugin ${NC}-----"
-git clone https://github.com/chrissicool/zsh-256color ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-256color
-echo "\n-----${RED} Install solarized-man ${NC}-----"
-git clone https://github.com/zlsun/solarized-man.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/solarized-man
-echo "\n-----${RED} Install Autosuggestions ${NC}-----"
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}plugins/zsh-autosuggestions
+# ZSH Setup
+if [ ! -d "~/.oh-my-zsh/custom/plugins/zsh-completions" ]; then
+  echo "\n-----${RED} Install zsh-completions ${NC}-----"
+  git clone https://github.com/zsh-users/zsh-completions ~/.oh-my-zsh/custom/plugins/zsh-completions
+fi
 
-### TODO: Symlink dotfiles
-ln -nf $HOME/dotfiles/zsh/bashrc $HOME/.bashrc
-ln -nf $HOME/dotfiles/zsh/zshrc $HOME/.zshrc
-ln -nf $HOME/dotfiles/zsh/oh-my-env.sh $HOME/.oh-my-env.sh
+if [ ! -d "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting" ]; then
+  echo "\n-----${RED} Install zsh-syntax-highlighting Plugin ${NC}-----"
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+fi
 
+if [ ! -d "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-dircolors-solarized" ]; then
+  echo "\n-----${RED} Install zsh dir-colours plugin ${NC}-----"
+  git clone --recursive -j8 git://github.com/joel-porquet/zsh-dircolors-solarized ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-dircolors-solarized
+fi
+
+if [ ! -d "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-256color" ]; then
+  echo "\n-----${RED} Install 256color ZSH Plugin ${NC}-----"
+  git clone https://github.com/chrissicool/zsh-256color ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-256color
+fi
+
+if [ ! -d "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/solarized-man" ]; then
+  echo "\n-----${RED} Install solarized-man ${NC}-----"
+  git clone https://github.com/zlsun/solarized-man.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/solarized-man
+fi
+
+if [ ! -d "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}plugins/zsh-autosuggestions" ]; then
+  echo "\n-----${RED} Install Autosuggestions ${NC}-----"
+  git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}plugins/zsh-autosuggestions
+fi
+
+
+# Symlink dotfiles
+echo "Symlink: bashrc" && ln -nf $HOME/dotfiles/zsh/bashrc $HOME/.bashrc
+echo "Symlink: zshrc" && ln -nf $HOME/dotfiles/zsh/zshrc $HOME/.zshrc
+echo "Symlink: oh-my-env" && ln -nf $HOME/dotfiles/zsh/oh-my-env.sh $HOME/.oh-my-env.sh
 
 # Node
+echo "Install nodejs"
 sudo apt install nodejs
 node install sass
 node install typescript
 node install webpack
 node install weback-cli
 
-
-## Alacritty
-# TODO:
-
-## Vim Setup
+# Vim Setup
+echo "Install Neovim and KemmojoVim-Setup"
 sudo apt install neovim
 curl -sL https://raw.githubusercontent.com/Kemmojo/KemojoVim/master/boot-KemojoVim.sh | sh
