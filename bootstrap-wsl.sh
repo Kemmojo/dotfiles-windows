e7aa40e5a119545b29bc17bd9bde3ee9f1e1ae53
Kemmojo <kemnadej80@yahoo.de>
2019-10-21 23:13:18 +0200

    fancy-index fix

--- bootstrap-wsl.sh
+++ bootstrap-wsl.sh
@@ -74,41 +74,43 @@ fi
 
 
 # Symlink dotfiles
-prompt "Create Directories & Symlink Dotfiles"
-## Home Directories
-echo "Create: ${GREEN}$HOME/Downloads${NC}" && mkdir -p $HOME/Downloads
-echo "Create: ${GREEN}$HOME/Documents${NC}" && mkdir -p $HOME/Documents
+if [ ! -f "$HOME/dotfiles/zsh/oh-my-env.sh $HOME/.oh-my-env.sh" ]; then
+  prompt "Create Directories & Symlink Dotfiles"
+  ## Home Directories
+  echo "Create: ${GREEN}$HOME/Downloads${NC}" && mkdir -p $HOME/Downloads
+  echo "Create: ${GREEN}$HOME/Documents${NC}" && mkdir -p $HOME/Documents
 
-## Development Directories
-echo "Create: ${GREEN}$WINDOWS_HOME/Code${NC}" && mkdir -p "$WINDOWS_HOME/Code"
-echo "Create: ${GREEN}$WINDOWS_HOME/Code/Webdev${NC}" && mkdir -p "$WINDOWS_HOME/Code/Webdev"
-echo "Create: ${GREEN}$WINDOWS_HOME/Code/Webdev/www" && mkdir -p "$WINDOWS_HOME/Code/Webdev/www"
-echo "Create: ${GREEN}$WINDOWS_HOME/Code/Windev${NC}" && mkdir -p "$WINDOWS_HOME/Code/Windev"
-echo "Create: ${GREEN}$WINDOWS_HOME/Code/Mobildev${NC}" && mkdir -p "$WINDOWS_HOME/Code/Mobildev"
-echo "Create: ${GREEN}$WINDOWS_HOME/Code/CLIdev${NC}" && mkdir -p "$WINDOWS_HOME/Code/CLIdev"
-echo "Create: ${GREEN}$WINDOWS_HOME/Code/Gamedev${NC}" && mkdir -p "$WINDOWS_HOME/Code/Gamedev"
+  ## Development Directories
+  echo "Create: ${GREEN}$WINDOWS_HOME/Code${NC}" && mkdir -p "$WINDOWS_HOME/Code"
+  echo "Create: ${GREEN}$WINDOWS_HOME/Code/Webdev${NC}" && mkdir -p "$WINDOWS_HOME/Code/Webdev"
+  echo "Create: ${GREEN}$WINDOWS_HOME/Code/Webdev/www" && mkdir -p "$WINDOWS_HOME/Code/Webdev/www"
+  echo "Create: ${GREEN}$WINDOWS_HOME/Code/Windev${NC}" && mkdir -p "$WINDOWS_HOME/Code/Windev"
+  echo "Create: ${GREEN}$WINDOWS_HOME/Code/Mobildev${NC}" && mkdir -p "$WINDOWS_HOME/Code/Mobildev"
+  echo "Create: ${GREEN}$WINDOWS_HOME/Code/CLIdev${NC}" && mkdir -p "$WINDOWS_HOME/Code/CLIdev"
+  echo "Create: ${GREEN}$WINDOWS_HOME/Code/Gamedev${NC}" && mkdir -p "$WINDOWS_HOME/Code/Gamedev"
 
-## Node Directories
-echo "Create: ${GREEN}/usr/local/lib/node_modules${NC}" && sudo mkdir -p /usr/local/lib/node_modules
+  ## Node Directories
+  echo "Create: ${GREEN}/usr/local/lib/node_modules${NC}" && sudo mkdir -p /usr/local/lib/node_modules
 
-## Symlinks
-echo "Symlink: bashrc" && ln -nf $HOME/dotfiles/zsh/bashrc $HOME/.bashrc
-echo "Symlink: zshrc" && ln -nf $HOME/dotfiles/zsh/zshrc $HOME/.zshrc
-echo "Symlink: oh-my-env" && ln -nf $HOME/dotfiles/zsh/oh-my-env.sh $HOME/.oh-my-env.sh
-echo "Symlink: gitconfig" && ln -nf $HOME/dotfiles/git/gitconfig $HOME/.gitconfig
-echo "\n"
-cd $HOME
+  ## Symlinks
+  echo "Symlink: bashrc" && ln -nf $HOME/dotfiles/zsh/bashrc $HOME/.bashrc
+  echo "Symlink: zshrc" && ln -nf $HOME/dotfiles/zsh/zshrc $HOME/.zshrc
+  echo "Symlink: oh-my-env" && ln -nf $HOME/dotfiles/zsh/oh-my-env.sh $HOME/.oh-my-env.sh
+  echo "Symlink: gitconfig" && ln -nf $HOME/dotfiles/git/gitconfig $HOME/.gitconfig
+  echo "\n"
+  cd $HOME
+fi
 
 
 # Node
 prompt "Install NodeJS / NPM / Basic Web development Packages"
 [ ! -x "$(command -v node)" ] && sudo apt install nodejs
 [ ! -x "$(command -v npm)" ] && sudo apt install npm
-sudo npm install -g sass
-sudo npm install -g typescript
-sudo npm install -g webpack
-sudo npm install -g webpack-cli
-sudo npm install -g @vue/cli
+[ ! -x "$(command -v sass)" ] && sudo npm install -g sass
+[ ! -x "$(command -v tsc)" ] && sudo npm install -g typescript
+[ ! -x "$(command -v webpack)" ] && sudo npm install -g webpack
+[ ! -x "$(command -v webpack-cli)" ] && sudo npm install -g webpack-cli
+[ ! -x "$(command -v vue)" ] && sudo npm install -g @vue/cli
 
 
 # Install Neovim + Setup KemmojoVim
@@ -116,11 +118,6 @@ prompt "Install Neovim & KemmojoVim-Setup"
 [ ! -x "$(command -v neovim)" ] && sudo apt install neovim
 echo "\n"
 curl -sL https://raw.githubusercontent.com/Kemmojo/KemojoVim/master/boot-KemojoVim.sh | sh
-
-
-# Change Shell to ZSH
-prompt "Change Default Shell"
-chsh -s /bin/zsh $USER
 
 
 # Install bat aka colored cat, aka cat with wings
@@ -163,9 +160,9 @@ sudo bash -c "echo 'xdebug.remote_enable = 1' >> /etc/php/*/apache2/php.ini"
 sudo bash -c "echo 'xdebug.remote_autostart = 1' >> /etc/php/*/apache2/php.ini"
 
 ## Install Fance Index
-if [ -d "/var/www/html/fancy-index" ]; then
+if [ ! -d "/var/www/html/fancy-index" ]; then
   prompt "Install Fancy-Index"
-  cd /var/www/html && git clone https://github.com/Vestride/fancy-index.git
+  cd /var/www/html && sudo git clone https://github.com/Vestride/fancy-index.git
   sudo mv /var/www/html/fancy-index/.htaccess /var/www/html
   sudo bash -c "echo '<Directory /var/www/html/>' >> /etc/apache2/apache2.conf"
   sudo bash -c "echo '    AllowOverride All' >> /etc/apache2/apache2.conf"
@@ -199,13 +196,13 @@ echo "Dude... Thanks for waiting. \n\n"
 echo "Have Fun. :)\n"
 echo "\n\n"
 echo "Here some things you might want to check out:"
-echo "- Adobe Photoshop: https://www.adobe.com/de/products/photoshop.html"
-echo "- Adobe XD: https://www.adobe.com/products/xd.html#"
-echo "- Affinity Photo: https://affinity.serif.com/en-gb/photo/"
-echo "- Flowkey: https://www.flowkey.com/en"
-echo "- XING: https://faq.xing.com/de/windows/xing-app-f%C3%BCr-windows-10-herunterladen"
-echo "- FL Studio: https://www.image-line.com/flstudio"
-echo "- ICUE: https://www.corsair.com/de/de/icue"
-echo "- Alesis V61 Editor: https://www.alesis.com/products/legacy/v61"
+echo "- ${GREEN}Adobe Photoshop:${NC}\t https://www.adobe.com/de/products/photoshop.html"
+echo "- ${GREEN}Adobe XD:${NC}\t\t https://www.adobe.com/products/xd.html#"
+echo "- ${GREEN}Affinity Photo:${NC}\t https://affinity.serif.com/en-gb/photo/"
+echo "- ${GREEN}Flowkey:${NC}\t\t https://www.flowkey.com/en"
+echo "- ${GREEN}XING:${NC}\t\t\t https://faq.xing.com/de/windows/xing-app-f%C3%BCr-windows-10-herunterladen"
+echo "- ${GREEN}FL Studio:${NC}\t\t https://www.image-line.com/flstudio"
+echo "- ${GREEN}ICUE:${NC}\t\t\t https://www.corsair.com/de/de/icue"
+echo "- ${GREEN}Alesis V61 Editor:${NC}\t https://www.alesis.com/products/legacy/v61"
 
 
